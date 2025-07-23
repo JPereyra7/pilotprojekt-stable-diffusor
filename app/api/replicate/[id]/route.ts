6ff237df4d0e62d8ca1dd5ee31e1adf4479ffdbb
JPereyra7 <joelpereyra1993@gmail.com>
2025-07-23 16:49:45 +0200

    :bug: GET _req added

--- app/api/replicate/[id]/route.ts
+++ app/api/replicate/[id]/route.ts
@@ -1,10 +1,16 @@
 import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(
-  _req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const res = await fetch(`https://api.replicate.com/v1/predictions/${params.id}`, {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(_req: NextRequest, context: Context) {
+  const id = context.params.id;
+
+  const res = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
     headers: {
       Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
       "Content-Type": "application/json",
@@ -14,8 +20,5 @@ export async function GET(
 
   const data = await res.json();
 
-  return new Response(JSON.stringify(data), {
-    status: res.status,
-    headers: { "Content-Type": "application/json" },
-  });
+  return NextResponse.json(data, { status: res.status });
 }
